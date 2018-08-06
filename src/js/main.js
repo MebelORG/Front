a9e9d685fb13be8ae39484a32bd6b979ab76b6ed
Snipeek <snipeek@gmail.com>
2018-08-06 11:46:40 +0300

    0.1.0

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+
+//= partials/old/dropzone.min.js
+
+//= partials/old/foundation.min.js
+
+//= partials/old/jquery.countTo.js
+
+//= partials/old/jquery.parallax-0.2.js
+
+//= partials/old/jquery.ui-slider.js
+
+//= partials/old/jqueryui.js
+
+//= partials/old/jquery.sticky-sidebar.min.js
+
+//= partials/old/script-2.js
+
+//= partials/old/setup.js
+
+//= partials/old/slick.min.js
+
+//= partials/old/slick.min.js
+
+
+$(document).ready(function(){
+
+});
