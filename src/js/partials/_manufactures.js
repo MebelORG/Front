02746c48f777876899f9856f2bbc415b6d0424f9
polinawork <32538950+polinawork@users.noreply.github.com>
2018-08-10 12:08:04 +0400

    //

--- src/js/partials/_manufactures.js
+++ src/js/partials/_manufactures.js
@@ -1,5 +1,4 @@
-
- ymaps.ready(init);
+ymaps.ready(init);
 
 function init(){ 
     var myMap = new ymaps.Map("map", {
@@ -24,93 +23,93 @@ function init(){
     myMap.behaviors.disable(['scrollZoom']);
 
     // Создадим собственный макет выпадающего списка.
-ListBoxLayout = ymaps.templateLayoutFactory.createClass(
-    "<div class='my-class'>" + "<button id='my-listbox-header' class='company-shops__choose-city' data-toggle='dropdown'>" +
-        "{{data.title}} <span></span>" +
-    "</button>" +
-    "<ul id='my-listbox'" +
-        " class='dropdown-menu company-shops__list' role='menu' aria-labelledby='dropdownMenu'" +
-        " style='display: {% if state.expanded %}block{% else %}none{% endif %};'></ul>", {
+    var ListBoxLayout = ymaps.templateLayoutFactory.createClass(
+        "<div class='my-class'>" + "<button id='my-listbox-header' class='company-shops__choose-city' data-toggle='dropdown'>" +
+            "{{data.title}} <span></span>" +
+        "</button>" +
+        "<ul id='my-listbox'" +
+            " class='dropdown-menu company-shops__list' role='menu' aria-labelledby='dropdownMenu'" +
+            " style='display: {% if state.expanded %}block{% else %}none{% endif %};'></ul>", {
 
-    build: function() {
-        ListBoxLayout.superclass.build.call(this);
+        build: function() {
+            ListBoxLayout.superclass.build.call(this);
 
-        this.childContainerElement = $('#my-listbox').get(0);
-        this.events.fire('childcontainerchange', {
-            newChildContainerElement: this.childContainerElement,
-            oldChildContainerElement: null
-        });
-    },
-    getChildContainerElement: function () {
-        return this.childContainerElement;
-    },
-
-    clear: function () {
-        this.events.fire('childcontainerchange', {
-            newChildContainerElement: null,
-            oldChildContainerElement: this.childContainerElement
-        });
-        this.childContainerElement = null;
-        ListBoxLayout.superclass.clear.call(this);
-    }
-}),
-
-ListBoxItemLayout = ymaps.templateLayoutFactory.createClass(
-    "<li><a>{{data.content}}</a></li>" + "</div>"
-),
-
-
-listBoxItems = [
-    new ymaps.control.ListBoxItem({
-        data: {
-            content: 'Санкт-Петербург',
-            center: [59.939521, 30.314930],
-            zoom: 12
-        }
-    }),
-
-    new ymaps.control.ListBoxItem({
-        data: {
-            content: 'Москва',
-            center: [55.751574, 37.573856],
-            zoom: 12
-        }
-    }),
-    new ymaps.control.ListBoxItem({
-        data: {
-            content: 'Омск',
-            center: [54.990215, 73.365535],
-            zoom: 12
-        }
-    })
-],
-
-listBox = new ymaps.control.ListBox({
-        items: listBoxItems,
-        data: {
-            title: 'Выберите город'
+            this.childContainerElement = $('#my-listbox').get(0);
+            this.events.fire('childcontainerchange', {
+                newChildContainerElement: this.childContainerElement,
+                oldChildContainerElement: null
+            });
         },
-        options: {
+        getChildContainerElement: function () {
+            return this.childContainerElement;
+        },
 
-            layout: ListBoxLayout,
+        clear: function () {
+            this.events.fire('childcontainerchange', {
+                newChildContainerElement: null,
+                oldChildContainerElement: this.childContainerElement
+            });
+            this.childContainerElement = null;
+            ListBoxLayout.superclass.clear.call(this);
+        }
+    }),
 
-            itemLayout: ListBoxItemLayout
+    ListBoxItemLayout = ymaps.templateLayoutFactory.createClass(
+        "<li><a>{{data.content}}</a></li>" + "</div>"
+    ),
+
+
+        listBoxItems = [
+        new ymaps.control.ListBoxItem({
+            data: {
+                content: 'Санкт-Петербург',
+                center: [59.939521, 30.314930],
+                zoom: 12
+            }
+        }),
+
+        new ymaps.control.ListBoxItem({
+            data: {
+                content: 'Москва',
+                center: [55.751574, 37.573856],
+                zoom: 12
+            }
+        }),
+        new ymaps.control.ListBoxItem({
+            data: {
+                content: 'Омск',
+                center: [54.990215, 73.365535],
+                zoom: 12
+            }
+        })
+    ],
+
+    listBox = new ymaps.control.ListBox({
+            items: listBoxItems,
+            data: {
+                title: 'Выберите город'
+            },
+            options: {
+
+                layout: ListBoxLayout,
+
+                itemLayout: ListBoxItemLayout
+            }
+    });
+
+    listBox.events.add('click', function (e) {
+
+        var item = e.get('target');
+
+        if (item != listBox) {
+            myMap.setCenter(
+                item.data.get('center'),
+                item.data.get('zoom')
+            );
         }
     });
 
-listBox.events.add('click', function (e) {
-
-    var item = e.get('target');
-
-    if (item != listBox) {
-        myMap.setCenter(
-            item.data.get('center'),
-            item.data.get('zoom')
-        );
-    }
-});
-
-myMap.controls.add(listBox, {float: 'left'});
+    myMap.controls.add(listBox, {float: 'left'});
 
 
 }
